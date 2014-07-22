f5b43b85e5dc5cd76bf327b94a8ae192c0684a68
Guy Halperin & Jeff Stringer <student@epicodus.com>
2014-07-22 15:36:05 -0700

    add spec to account for moving hour hand

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -4,9 +4,10 @@ def clock_angle(time)
   if time[0]==12.0
     time[0]=0
   end
-  hour_degrees = time[0]*30
+  minutes_add = (time[1]/60)*30
+  hour_degrees = time[0]*30 + minutes_add
   minutes_degrees = time[1]*6
-  degrees = hour_degrees-minutes_degrees
+  degrees = (hour_degrees-minutes_degrees).abs
 end
 
 clock_angle("6:00")
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -6,7 +6,19 @@ describe('clock_angle') do
     expect(clock_angle("6:00")).to eq(180)
   end
 
-  it("takes the time and measures distance from hour hand to minute and converts to degrees fpr 0 deg") do
+  it("takes the time and measures distance from hour hand to minute and converts to degrees for 0 deg") do
     expect(clock_angle("12:00")).to eq(0)
+  end
+
+  it("takes the time and measures distance from hour hand to minute and accounts for hour hand changes") do
+    expect(clock_angle("6:15")).to eq(97.5)
+  end
+
+  it("takes the time and measures distance from hour hand to minute and accounts for hour hand changes") do
+    expect(clock_angle("7:45")).to eq(37.5)
+  end
+
+  it("takes the time and measures distance from hour hand to minute and accounts for hour hand changes") do
+    expect(clock_angle("12:15")).to eq(82.5)
   end
 end
